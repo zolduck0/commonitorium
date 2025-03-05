69c9a19c784290e60ab4e789a9438184311a7255
zolduck <arthurswitch3@gmail.com>
2025-03-05 18:17:55 -0300

    script finished

--- commonitorium.cpp
+++ commonitorium.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
 #include <cstdlib>
 #include <unistd.h>
+#include <signal.h>
+#include <atomic>
+
+std::atomic<bool> keepRunning(true);
+
+void signalHandler(int signum)
+{
+    std::cout << "Interrupt signal (" << signum << ") received.\n";
+    keepRunning = false;
+}
 
 int main()
 {
-    while (true)
+    signal(SIGTERM, signalHandler);
+
+    while(keepRunning)
     {
         system("gnome-terminal -- bash -c './terminal.sh'");
         sleep(3600);
     }
+
+    std::cout << "Exiting...\n";
 
     return 0;
 }
--- terminal.sh
+++ terminal.sh
@@ -4,10 +4,14 @@ echo "Are u still workin? :3"
 
 read -p "-> " input
 
-if [ "$input" == "y" ]; then
+if [ "$input" == "shut up" ]; then
+    echo "terminating this shit!!!!!!!!!!!"
+    kill $PPID
+    exit 0
+elif [ "$input" == "y" ]; then
     echo "good boi!!!"
+    exit 0
 else
     echo "kill yourself"
+    exec bash
 fi
-
-exec bash
